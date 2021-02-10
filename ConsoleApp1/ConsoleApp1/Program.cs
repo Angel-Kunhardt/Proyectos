22d0e0d21262ae2ee751592b867e6410927976d0
Angel-Kunhardt <angelkunhardt21@gmail.com>
2021-02-10 19:56:30 -0400

    proyecto

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ejercicios
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine("Los 10 primeros numeros pares a partir del producto de 10 * 10 son:");
+            for (i= 102; i <= 120; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
